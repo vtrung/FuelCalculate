7147f40a5e3be0d06691303c894e0af7cbb6e693
Ving Trung <vtrung>
2017-10-19 00:46:35 -0700

    added api endpoints to get mongo data

--- api/index.js
+++ api/index.js
@@ -8,18 +8,54 @@ router.get('/', (req, res) => {
 })
 
 router.get('/cars/years', (req, res) => {
-    var carlist = {};
-
-    // Car.find(function(error, ids) {
-    //     // ids is an array of all ObjectIds
-    //     res.send(ids);
-    // });
-
-    Car.find().distinct("year", function(error, ids) {
+    Car.find().distinct("year", (error, years) => {
         // ids is an array of all ObjectIds
-        res.send(ids);
+        res.send(years);
     });
 })
 
+router.get('/cars/id/:id', (req, res) =>{
+    Car.find(
+        {
+            '_id':req.params['id']
+        },
+        (err, cars) => {
+            res.send(cars);
+        }
+    )
+})
+
+router.get('/cars/year/:year', (req, res) =>{
+    Car.find({'year':req.params['year']})
+    .distinct("make", (error, makes) => {
+        // ids is an array of all ObjectIds
+        res.send(makes);
+    });
+})
+
+router.get('/cars/year/:year/make/:make', (req, res) =>{
+    Car.find(
+        {
+            'year':req.params['year'],
+            'make':req.params['make']
+        }
+    )
+    .distinct("model", (error, models) => {
+        res.send(models);
+    });
+})
+
+router.get('/cars/year/:year/make/:make/model/:model', (req, res) =>{
+    Car.find(
+        {
+            'year':req.params['year'],
+            'make':req.params['make'],
+            'model':req.params['model']
+        },
+        (err, cars) => {
+            res.send(cars);
+        }
+    )
+})
 
 export default router;
